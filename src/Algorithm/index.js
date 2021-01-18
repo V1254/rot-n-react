c8d35847f942bc0c9b49c3a44f78ce98406ddd05
Mo <33751344+V1254@users.noreply.github.com>
2021-01-18 18:21:19 +0000

    cleanup algo

--- src/Algorithm/index.js
+++ src/Algorithm/index.js
@@ -1,13 +1,8 @@
-const encryptText = (text, shift) => {
-  console.log(`received: ${text} and ${shift} | typofShift: ${typeof shift}`);
-
-  const result = text
+const encryptText = (text, shift) =>
+  text
     .split("")
     .map((char) => {
       if (!char.match(/[a-z]/i)) return char;
-
-      console.log(`char is: ${char}`);
-
       let startIndex = 97;
       if (char === char.toUpperCase()) {
         startIndex = 65;
@@ -18,19 +13,12 @@ const encryptText = (text, shift) => {
       let nextCharCode = char.charCodeAt(0) + shift;
 
       if (nextCharCode > maxIndex) {
-        console.log(`entered if...`);
         nextCharCode = startIndex + (nextCharCode - maxIndex) - 1;
       }
 
       const rtn = String.fromCharCode(nextCharCode);
-      // console.log(`Algorith: current: ${char} | next: ${nextCharCode} | nextChar: ${rtn}`);
-
       return rtn;
     })
     .join("");
-
-  console.log(`encrypted as: ${result} from ${text} using rot-${shift}`);
-  return result;
-};
 
 export default encryptText;
